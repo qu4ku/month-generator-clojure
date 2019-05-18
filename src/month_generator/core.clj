36c36c984ed4e82c4670dbbfb765b512e2123a56
qu4ku <qu4ku@hotmail.com>
2019-05-18 19:09:19 +0200

    month generator

--- src/month_generator/core.clj
+++ src/month_generator/core.clj
@@ -1,7 +1,30 @@
 (ns month-generator.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+; (defn -main
+;   "I don't do a whole lot ... yet."
+;   [& args]
+;   (println "Hello, World!"))
+
+(def one-day (apply str (repeat 18 "_  ")))
+
+(defn generate-weekdays [first-sat]  
+  (sort (concat 
+          (range first-sat 32 7)
+          (range (inc first-sat) 32 7))))
+
+(defn is-weekend? [day weekdays]
+  "Takes day and generated weekdays and returns proper part of a string"
+  (if (some #{day} weekdays)
+    "|\t"
+    "\t"))
+
+(defn generate-month [num-days first-sat month year]
+  (let [weekdays (generate-weekdays first-sat)]
+    (doseq [day (range 1 (inc num-days))]
+      (println (apply str 
+                 (format "%d.%02d.%02d" year month day) 
+                 (is-weekend? day weekdays) 
+                 one-day)))))
+
+(generate-month 31 4 5 19)
